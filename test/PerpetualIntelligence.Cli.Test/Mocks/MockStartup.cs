8eb2094e4c7247ed4cdc910d689895b90777b94b
Amey Subhedar <ameyasubhedar@gmail.com>
2022-05-09 14:47:24 -0700

    update lic

--- test/PerpetualIntelligence.Cli.Test/Mocks/MockStartup.cs
+++ test/PerpetualIntelligence.Cli.Test/Mocks/MockStartup.cs
@@ -1,7 +1,8 @@
 /*
-    Copyright (c) Perpetual Intelligence L.L.C. All Rights Reserved
-    https://perpetualintelligence.com
-    https://api.perpetualintelligence.com
+    Copyright (c) Perpetual Intelligence L.L.C. All Rights Reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com
 */
 
 using Microsoft.Extensions.DependencyInjection;
