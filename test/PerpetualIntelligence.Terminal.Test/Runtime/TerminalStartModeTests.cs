1ab412f609701932c7474a014c22a307446327a4
Amey Subhedar <dev@perpetualintelligence.com>
2023-06-19 23:12:14 -0700

    Update start context

---
+++ test/PerpetualIntelligence.Terminal.Test/Runtime/TerminalStartModeTests.cs
@@ -0,0 +1,27 @@
+﻿/*
+    Copyright (c) 2021 Perpetual Intelligence L.L.C. All Rights Reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com/articles/intro.html
+*/
+
+using FluentAssertions;
+using Xunit;
+
+namespace PerpetualIntelligence.Terminal.Runtime
+{
+    public class TerminalStartModeTests
+    {
+        [Fact]
+        public void StartModes_Has_Correct_EnumValues()
+        {
+            ((int)TerminalStartMode.Tcp).Should().Be(1);
+            ((int)TerminalStartMode.Http).Should().Be(2);
+            ((int)TerminalStartMode.Grpc).Should().Be(3);
+
+            ((int)TerminalStartMode.Console).Should().Be(100);
+
+            ((int)TerminalStartMode.Custom).Should().Be(0);
+        }
+    }
+}
