c7a4f6eefb894787a052628da6d2a383c978e1ac
Dev Team <dev@perpetualintelligence.com>
2023-08-11 21:33:40 -0700

    update tests

--- src/PerpetualIntelligence.Terminal/Commands/ArgumentDescriptor.cs
+++ src/PerpetualIntelligence.Terminal/Commands/ArgumentDescriptor.cs
@@ -16,14 +16,31 @@ namespace PerpetualIntelligence.Terminal.Commands
         /// <summary>
         /// Initializes a new instance of <see cref="ArgumentDescriptor"/>.
         /// </summary>
+        /// <param name="order">The argument order.</param>
         /// <param name="id">The argument identifier.</param>
         /// <param name="dataType">The argument data type.</param>
         /// <param name="description">The argument description.</param>
-        public ArgumentDescriptor(string id, string dataType, string description)
+        public ArgumentDescriptor(int order, string id, string dataType, string description)
         {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new System.ArgumentException($"'{nameof(id)}' cannot be null or empty.", nameof(id));
+            }
+
+            if (string.IsNullOrEmpty(dataType))
+            {
+                throw new System.ArgumentException($"'{nameof(dataType)}' cannot be null or empty.", nameof(dataType));
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new System.ArgumentException($"'{nameof(description)}' cannot be null or empty.", nameof(description));
+            }
+
             Id = id;
             DataType = dataType;
             Description = description;
+            Order = order;
         }
 
         /// <summary>
@@ -40,5 +57,10 @@ namespace PerpetualIntelligence.Terminal.Commands
         /// The argument description.
         /// </summary>
         public string Description { get; }
+
+        /// <summary>
+        /// The argument order.
+        /// </summary>
+        public int Order { get; }
     }
 }
