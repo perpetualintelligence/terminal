d7117ca626e2cf9be4377583b5ee83f6e4f578cf
pi.admin <orgadmin@perpetualintelligence.com>
2024-10-14 19:45:24 -0700

    Update package version

---
+++ test/OneImlx.Terminal.Client.Tests/AssemblyTests.cs
@@ -0,0 +1,36 @@
+﻿/*
+    Copyright © 2019-2025 Perpetual Intelligence L.L.C. All rights reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com/articles/intro.html
+*/
+
+using FluentAssertions;
+using OneImlx.Terminal.Client.Extensions;
+using OneImlx.Test.FluentAssertions;
+using Xunit;
+
+namespace OneImlx.Terminal.Client
+{
+    public class AssemblyTests
+    {
+        [Fact]
+        public void TypesLocationTest()
+        {
+            typeof(TerminalGrpcRouterProtoClientExtensions).Assembly.Should().HaveTypesInValidLocations(
+                [
+                    typeof(OneimlxTerminalReflection),
+                    typeof(TerminalGrpcRouterProtoInput),
+                    typeof(TerminalGrpcRouterProtoOutput),
+                    typeof(TerminalGrpcRouterProto),
+                    typeof(TerminalGrpcRouterProto.TerminalGrpcRouterProtoClient),
+                ]);
+        }
+
+        [Fact]
+        public void TypesNamespaceTest()
+        {
+            typeof(TerminalGrpcRouterProtoClientExtensions).Assembly.Should().HaveTypesInRootNamespace("OneImlx.Terminal.Client");
+        }
+    }
+}
